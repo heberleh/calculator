138de229739bfeddaac5ca027369c3d9b89e01f3
Henry Heberle <heberleh.public@gmail.com>
2019-05-17 14:10:01 -0300

    Calculator working

--- calculator.js
+++ calculator.js
@@ -1,23 +1,34 @@
 
 
 
-let lastOperate = false;
-let lastEqual = false;
+
+const EQUAL = 0;
+const OPERATE = 1;
+const NUMBER = 2;
+let lastAction = null;
 let lastOperator = null;
 var a = null;
 var b = null;
+
+clear();
+// remove press-button effect
+document.querySelectorAll(".button")
+    .forEach((element) =>{
+        element.addEventListener('transitionend',(event) =>{
+            event.target.classList.remove("playing");
+        });
+    });
+
+// add press-button effect
+document.querySelectorAll(".button")
+    .forEach((element)=>{element.addEventListener('click',pressButton);});
 
 
 // Typing numbers and updating screen
 document.querySelectorAll(".number")
         .forEach((element) =>{
             element.addEventListener("click", (event)=>{                
-                let node = event.target;
-
-                if (node.tagName == "P") {node.parentNode.classList.add("playing");}
-                else {node.classList.add("playing");}
-
-                if (node.tagName == "DIV") {node = node.firstElementChild;}
+                let node = getValueNode(event);
 
                 let digit = node.innerHTML;
                 let display_node = document.querySelector("#display-value");
@@ -28,18 +39,12 @@ document.querySelectorAll(".number")
                 if (parseFloat(display_value) == 0 && digit == '0'){ return;}
                 if (display_value == "" && digit == '.'){display_value = '0';}
 
-                if (lastOperate || lastEqual){
+                if (lastAction != NUMBER){
                     display_node.innerHTML = digit;
                 }else{
                     display_node.innerHTML = display_value + digit;
-                }                
-                
-                lastOperate = false;
-                lastEqual = true;
-            });
-
-            element.addEventListener('transitionend',(event) =>{
-                event.target.classList.remove("playing");
+                }                            
+                lastAction = NUMBER;                
             });
         });
 
@@ -52,47 +57,76 @@ document.querySelector("#clear")
 document.querySelectorAll(".operator")
         .forEach((element)=>{
             element.addEventListener("click",(e) =>{
-                let node = e.target;
-
-                if (node.tagName == "P") {node.parentNode.classList.add("playing");}
-                else {node.classList.add("playing");}
-
-                if (node.tagName == "DIV") {node = node.firstElementChild;}
+                let node = getValueNode(event);
 
                 let operator = node.innerHTML;
                 let display_node = document.querySelector("#display-value");
                 let display_value = display_node.innerHTML;
 
-                if (lastOperate){return;}
-                else{lastOperate = true;}
+                if (lastAction == OPERATE){return;}
+                else{lastAction = OPERATE;}
                             
                 a = b;
                 b = parseFloat(display_value);
 
-                console.log(a, b, operator, lastOperator);
+                //console.log(a, b, operator, lastOperator);                   
 
                 let result = null;
                 if (a!=null && b!=null){
-                    if(operator == "Enter"){
-                        if(lastOperator != null){
-                            result = operate(lastOperator);
-                            lastOperator = null;    
-                        }
+                    if (lastAction == EQUAL){
+                        return;
                     }else{
-                        result = operate(operator);
+                        result = operate(lastOperator);
                         lastOperator = operator;
-                    }                    
+                        b = result;
+                    }
                 }else{
-                    result = b;
+                    lastOperator = operator;                                        
+                    return;
                 }
 
-                display_node.innerHTML = result;                
-            });
-
-            element.addEventListener('transitionend',(event) =>{
-                event.target.classList.remove("playing");
+                // if did not return but calculated the result, show it
+                updateDisplay(result);                
             });
         });
+
+
+let enter = document.querySelector("#enter");
+enter.addEventListener("click",(e) =>{    
+
+    if (lastAction == EQUAL) return;
+
+    let display_node = document.querySelector("#display-value");
+    let display_value = display_node.innerHTML;
+
+    // compute
+    a = b;
+    b = parseFloat(display_value);
+    let result = operate(lastOperator);
+
+    //console.log(a, b, result);
+    // update control vars
+    lastAction = EQUAL;
+    a = null;
+    b = null;
+
+    // show
+    updateDisplay(result); 
+});
+
+function pressButton(event){
+    // button action
+    let node = event.target;
+    if (node.tagName == "P") {node.parentNode.classList.add("playing");}
+    else {node.classList.add("playing");}
+}
+
+function getValueNode(event){
+    // return the p node
+    let node = event.target;
+    if (node.tagName == "DIV") {node = node.firstElementChild;}
+    return node;
+}
 
 
 function operate(operator){
@@ -104,6 +138,12 @@ function operate(operator){
     }
 }
 
+function updateDisplay(value){    
+    if (/\d*$/.exec(String(value))[0].length > 4){
+        value = value.toFixed(4)
+    }
+    document.querySelector("#display-value").innerHTML = value;
+}
         
 function clear(){
     document.querySelector("#display-value").innerHTML = "";
@@ -112,6 +152,7 @@ function clear(){
     lastOperate = false;
     lastEqual = false;
     lastOperator = null;
+    updateDisplay(0.0); 
 }
 
 
